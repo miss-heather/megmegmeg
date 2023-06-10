1bc03096c0cd3e16077ef07b9f1b787e4e2a7d62
MissHeather <heather.hawkes808@gmail.com>
2023-06-10 16:52:07 -0700

    ridesharing

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react
 import { EmergencyContactsProvider } from './EmergencyContactsContext';
 import './css/emergencyContacts.css';
 import './css/Diary.css';
+import './css/Rideshare.css';
 import Home from './Home';
 import EmergencyContacts from './EmergencyContacts';
 import LiveLocation from './LiveLocation';
@@ -82,10 +83,76 @@ const Navigation = () => {
   );
 };
 
-const Rideshare = () => {
+const RidesharePage = () => {
+  const [images, setImages] = React.useState([]);
+  const [sendToEmergencyContact, setSendToEmergencyContact] = React.useState(false);
+  const [emergencyContact, setEmergencyContact] = React.useState('');
+
+  const handleImageUpload = (event) => {
+    const fileList = event.target.files;
+    const newImages = Array.from(fileList);
+    setImages((prevImages) => [...prevImages, ...newImages]);
+  };
+
+  const handleSendToEmergencyContactChange = (event) => {
+    setSendToEmergencyContact(event.target.checked);
+  };
+
+  const handleEmergencyContactChange = (event) => {
+    setEmergencyContact(event.target.value);
+  };
+
   return (
-    <div>
-      <h1>MeG ~ My eGuardian</h1>
+    <div className="rideshare-page">
+      <h3>Ridesharing?</h3>
+
+      <label htmlFor="name">Name:</label>
+      <input type="text" id="name" />
+
+      <label htmlFor="username">User Name:</label>
+      <input type="text" id="username" />
+
+      <label htmlFor="license">License Plate #:</label>
+      <input type="text" id="license" />
+
+      <label htmlFor="color">Color:</label>
+      <input type="text" id="color" />
+
+      <label htmlFor="yearMakeModel">Year/Make/Model:</label>
+      <input type="text" id="yearMakeModel" />
+
+      <label htmlFor="images">Upload Images:</label>
+      <input type="file" id="images" accept="image/*" multiple onChange={handleImageUpload} />
+
+      {images.length > 0 && (
+        <div>
+          <h4>Uploaded Images:</h4>
+          {images.map((image, index) => (
+            <div key={index}>
+              <img src={URL.createObjectURL(image)} alt={`Uploaded Image ${index + 1}`} />
+            </div>
+          ))}
+        </div>
+      )}
+
+      <div>
+        <label htmlFor="sendToEmergencyContact">
+          Send Rideshare Data to Emergency Contact:
+          <input
+            type="checkbox"
+            id="sendToEmergencyContact"
+            checked={sendToEmergencyContact}
+            onChange={handleSendToEmergencyContactChange}
+          />
+        </label>
+      </div>
+
+      {sendToEmergencyContact && (
+        <div>
+          <label htmlFor="emergencyContact">Emergency Contact:</label>
+          <input type="text" id="emergencyContact" value={emergencyContact} onChange={handleEmergencyContactChange} />
+        </div>
+      )}
     </div>
   );
 };
@@ -103,7 +170,7 @@ const Root = () => {
           <Route path="/panic-features" element={<PanicFeatures />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<SignIn />} />
-          <Route path="/rideshare" element={<Rideshare />} />
+          <Route path="/rideshare" element={<RidesharePage />} />
         </Routes>
       </div>
     </Router>
