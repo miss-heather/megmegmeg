222d42c29733271f1676ae0ee50cff39a3e9270a
MissHeather <heather.hawkes808@gmail.com>
2023-06-10 17:57:14 -0700

    ridesharing emergency

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import { EmergencyContactsProvider } from './EmergencyContactsContext';
@@ -84,14 +84,19 @@ const Navigation = () => {
 };
 
 const RidesharePage = () => {
-  const [images, setImages] = React.useState([]);
-  const [sendToEmergencyContact, setSendToEmergencyContact] = React.useState(false);
-  const [emergencyContact, setEmergencyContact] = React.useState('');
+  const [sendToEmergencyContact, setSendToEmergencyContact] = useState(false);
+  const [emergencyContact, setEmergencyContact] = useState('');
+  const [destinationAddress, setDestinationAddress] = useState('');
+  const [parameterRadius, setParameterRadius] = useState('');
+  const [images, setImages] = useState([]);
+  const [setupLocationParameters, setSetupLocationParameters] = useState(false);
+  const [notifyEmergencyContact, setNotifyEmergencyContact] = useState(false);
+  const [emergencyContactInfo, setEmergencyContactInfo] = useState('');
 
   const handleImageUpload = (event) => {
-    const fileList = event.target.files;
-    const newImages = Array.from(fileList);
-    setImages((prevImages) => [...prevImages, ...newImages]);
+    const files = event.target.files;
+    // Handle the uploaded files here, such as sending them to a server or processing them.
+    setImages(Array.from(files));
   };
 
   const handleSendToEmergencyContactChange = (event) => {
@@ -102,27 +107,48 @@ const RidesharePage = () => {
     setEmergencyContact(event.target.value);
   };
 
+  const handleDestinationAddressChange = (event) => {
+    setDestinationAddress(event.target.value);
+  };
+
+  const handleParameterRadiusChange = (event) => {
+    setParameterRadius(event.target.value);
+  };
+
+  const handleSetupLocationParametersChange = (event) => {
+    setSetupLocationParameters(event.target.checked);
+  };
+
+  const handleNotifyEmergencyContactChange = (event) => {
+    setNotifyEmergencyContact(event.target.checked);
+  };
+
+  const handleEmergencyContactInfoChange = (event) => {
+    setEmergencyContactInfo(event.target.value);
+  };
+
   return (
     <div className="rideshare-page">
       <h3>Ridesharing?</h3>
 
       <label htmlFor="name">Name:</label>
       <input type="text" id="name" />
-
+        <br/>
       <label htmlFor="username">User Name:</label>
       <input type="text" id="username" />
-
+        <br/>
       <label htmlFor="license">License Plate #:</label>
       <input type="text" id="license" />
-
+        <br/>
       <label htmlFor="color">Color:</label>
       <input type="text" id="color" />
-
+        <br/>
       <label htmlFor="yearMakeModel">Year/Make/Model:</label>
       <input type="text" id="yearMakeModel" />
-
+        <br/>
       <label htmlFor="images">Upload Images:</label>
       <input type="file" id="images" accept="image/*" multiple onChange={handleImageUpload} />
+        <br/>
 
       {images.length > 0 && (
         <div>
@@ -151,6 +177,61 @@ const RidesharePage = () => {
         <div>
           <label htmlFor="emergencyContact">Emergency Contact:</label>
           <input type="text" id="emergencyContact" value={emergencyContact} onChange={handleEmergencyContactChange} />
+        </div>
+      )}
+
+      <label htmlFor="destinationAddress">Destination Address:</label>
+      <input type="text" id="destinationAddress" value={destinationAddress} onChange={handleDestinationAddressChange} />
+
+      <div>
+        <label htmlFor="setupLocationParameters">
+          Set up Location Parameters for Safe Arrival:
+          <input type="checkbox" id="setupLocationParameters" onChange={handleSetupLocationParametersChange} />
+        </label>
+      </div>
+
+      {setupLocationParameters && (
+        <>
+          <label htmlFor="parameterRadius">Parameter Radius (in miles):</label>
+          <input
+            type="number"
+            id="parameterRadius"
+            value={parameterRadius}
+            onChange={handleParameterRadiusChange}
+            step="0.1"
+            min="0"
+          />
+
+          <div>
+            <label htmlFor="notifyEmergencyContact">
+              Notify Emergency Contact if Destination is not Reached:
+              <input
+                type="checkbox"
+                id="notifyEmergencyContact"
+                checked={notifyEmergencyContact}
+                onChange={handleNotifyEmergencyContactChange}
+              />
+            </label>
+          </div>
+
+          {notifyEmergencyContact && (
+            <div>
+              <label htmlFor="emergencyContactInfo">Emergency Contact Info:</label>
+              <input
+                type="text"
+                id="emergencyContactInfo"
+                value={emergencyContactInfo}
+                onChange={handleEmergencyContactInfoChange}
+              />
+            </div>
+          )}
+        </>
+      )}
+
+      {!setupLocationParameters && (
+        <div style={{ display: 'none' }}>
+          <label htmlFor="parameterRadius">Parameter Radius (in miles):</label>
+          <input type="number" id="parameterRadius" value={parameterRadius} onChange={handleParameterRadiusChange} />
         </div>
       )}
     </div>
